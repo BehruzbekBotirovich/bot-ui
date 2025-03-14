c94ec39bedd7e8599b57115892718b90ef1734eb
Your Name <behruzbekbotirovich@gmail.com>
2025-03-14 20:35:59 +0500

    tg init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { onMounted, ref } from 'vue';
 
+const tg = window.Telegram.WebApp; // Получаем объект Telegram Web App
+const user = ref(null);
+
+onMounted(() => {
+  tg.expand(); // Разворачиваем Web App на весь экран
+  user.value = tg.initDataUnsafe?.user || null; // Получаем данные пользователя из Telegram
+});
 </script>
 
 <template>
